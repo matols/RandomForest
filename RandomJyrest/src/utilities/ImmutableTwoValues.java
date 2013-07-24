cb3dcea411368f0dd961226738e31e9dadad3a1c
unknown <SimonCB765@gmail.com>
2013-07-24 22:00:38 +0100

    Class to enable returning two values

---
+++ RandomJyrest/src/utilities/ImmutableTwoValues.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package utilities;
+
+/**
+ * @author Simon Bull
+ */
+public final class ImmutableTwoValues<S, T>
+{
+
+	public final S first;
+	public final T second;
+
+	public ImmutableTwoValues(S first, T second)
+	{
+		this.first = first;
+		this.second = second;
+	}
+
+}
