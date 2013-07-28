0b05df06ac2737a4e6c2e3ec570cf2d9c0edb58e
Simon Bull <SimonCB765@gmail.com>
2013-07-28 23:43:38 +0100

    Weight and mtry optimisation implemented

---
+++ RandomJyrest/src/analysis/PredictionAnalysis.java
@@ -0,0 +1,222 @@
+package analysis;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public final class PredictionAnalysis
+{
+	
+	/**
+	 * @param observationClasses
+	 * @param predictions
+	 * @return
+	 */
+	public static final Map<String, Map<String, Integer>> calculateConfusionMatrix(List<String> observationClasses, Map<String, double[]> predictions)
+	{
+		// Set up the confusion matrix.
+		Map<String, Map<String, Integer>> confusionMatrix = new HashMap<String, Map<String, Integer>>();
+		for (String s : predictions.keySet())
+		{
+			Map<String, Integer> classPredictions = new HashMap<String, Integer>();
+			classPredictions.put("Correct", 0);
+			classPredictions.put("Incorrect", 0);
+			confusionMatrix.put(s, classPredictions);
+		}
+
+		int numberOfObservations = observationClasses.size();
+		for (int i = 0; i < numberOfObservations; i++)
+		{
+			String obsClass = observationClasses.get(i);
+			String predictedClass = "";
+			double maxPredictedWeightForObs = 0.0;
+			for (Map.Entry<String, double[]> entry : predictions.entrySet())
+			{
+				String classOfPrediction = entry.getKey();
+				double predictedClassWeight = entry.getValue()[i];
+				if (predictedClassWeight > maxPredictedWeightForObs)
+				{
+					predictedClass = classOfPrediction;
+					maxPredictedWeightForObs = predictedClassWeight;
+				}
+			}
+			
+			// Update the confusion matrix.
+			if (obsClass.equals(predictedClass))
+			{
+				// Correctly predicted the class of an observation.
+				Integer oldCount = confusionMatrix.get(obsClass).get("Correct");
+				confusionMatrix.get(obsClass).put("Correct", oldCount + 1);
+			}
+			else
+			{
+				// Incorrectly predicted the class of the observation.
+				Integer oldCount = confusionMatrix.get(predictedClass).get("Incorrect");
+				confusionMatrix.get(predictedClass).put("Incorrect", oldCount + 1);
+			}
+		}
+		
+		return confusionMatrix;
+	}
+	
+	/**
+	 * @param confusionMatrix
+	 * @return
+	 */
+	public static final double calculateAccuracy(Map<String, Map<String, Integer>> confusionMatrix)
+	{
+		double correctPredictions = 0.0;
+		double totalPredictions = 0.0;
+		for (Map.Entry<String, Map<String, Integer>> entry : confusionMatrix.entrySet())
+		{
+			Map<String, Integer> predictions = entry.getValue();
+			double correct = predictions.get("Correct");
+			double incorrect = predictions.get("Incorrect");
+			correctPredictions += correct;
+			totalPredictions += correct + incorrect;
+		}
+		return correctPredictions / totalPredictions;
+	}
+	
+	/**
+	 * @param confusionMatrix
+	 * @param observationClasses
+	 * @param fType
+	 * @return
+	 */
+	public static final double calculateFMeasure(Map<String, Map<String, Integer>> confusionMatrix, List<String> observationClasses, double fType)
+	{
+		Map<String, Integer> countsOfEachClass = new HashMap<String, Integer>();
+		for (String s : confusionMatrix.keySet())
+		{
+			countsOfEachClass.put(s, Collections.frequency(observationClasses, s));
+		}
+		
+		double recall = 0.0;
+		double precision = 0.0;
+		for (Map.Entry<String, Map<String, Integer>> entry : confusionMatrix.entrySet())
+		{
+			Map<String, Integer> predictions = entry.getValue();
+			double correct = predictions.get("Correct");
+			double incorrect = predictions.get("Incorrect");
+			recall += correct / countsOfEachClass.get(entry.getKey());
+			precision += correct / (correct + incorrect);
+		}
+		double fMeasure = (1 + (fType * fType)) * ((precision * recall) / ((2 * 2 * precision) + recall));
+		return fMeasure;
+	}
+	
+	/**
+	 * @param confusionMatrix
+	 * @param observationClasses
+	 * @return
+	 */
+	public static final double calculateGMean(Map<String, Map<String, Integer>> confusionMatrix, List<String> observationClasses)
+	{
+		Map<String, Integer> countsOfEachClass = new HashMap<String, Integer>();
+		for (String s : confusionMatrix.keySet())
+		{
+			countsOfEachClass.put(s, Collections.frequency(observationClasses, s));
+		}
+		
+		double gMean = 1.0;
+		for (Map.Entry<String, Map<String, Integer>> entry : confusionMatrix.entrySet())
+		{
+			Map<String, Integer> predictions = entry.getValue();
+			double correct = predictions.get("Correct");
+			double recall = correct / countsOfEachClass.get(entry.getKey());
+			gMean *= recall;
+		}
+		gMean = Math.pow(gMean, (1.0 / confusionMatrix.size()));
+		return gMean;
+	}
+	
+	/**
+	 * @param confusionMatrix
+	 * @return
+	 */
+	public static final double calculateMCC(Map<String, Map<String, Integer>> confusionMatrix)
+	{
+		double MCC = Double.NaN;
+		List<String> allClasses = new ArrayList<String>(confusionMatrix.keySet());
+		if (allClasses.size() == 2)
+		{
+			String classOne = allClasses.get(0);
+			String classTwo = allClasses.get(1);
+			double TP = confusionMatrix.get(classOne).get("Correct");
+			double FP = confusionMatrix.get(classOne).get("Incorrect");
+			double TN = confusionMatrix.get(classTwo).get("Correct");
+			double FN = confusionMatrix.get(classTwo).get("Incorrect");
+			MCC = ((TP * TN) - (FP * FN)) / (Math.sqrt((TP + TN) * (TP + FN) * (TN + FP) * (TN + FN)));
+		}
+		return MCC;
+	}
+
+	/**
+	 * @param inputFile
+	 * @return
+	 */
+	public static final List<String> determineClassOfObservations(String inputFile)
+	{
+		List<String> classOfObservations = new ArrayList<String>();
+		Path dataPath = Paths.get(inputFile);
+		try (BufferedReader reader = Files.newBufferedReader(dataPath, StandardCharsets.UTF_8))
+		{
+			String line = null;
+
+			// Determine the column that contains the class data.
+			line = reader.readLine();
+			line = line.replaceAll("\n", "");
+			String[] featureNames = line.split("\t");
+			String classFeatureColumnName = "Classification";
+			int classIndex = -1;
+			int featureIndex = 0;
+			for (String feature : featureNames)
+			{
+				if (feature.equals(classFeatureColumnName))
+				{
+					classIndex = featureIndex;
+				}
+				featureIndex++;
+			}
+			if (classIndex == -1)
+			{
+				// No class column was provided.
+				System.out.println("No class column was provided. Please include a column headed Classification.");
+				System.exit(0);
+			}
+			
+			// Extract the class data.
+			while ((line = reader.readLine()) != null)
+			{
+				line = line.trim();
+				if (line.length() == 0)
+				{
+					// If the line is made up of all whitespace, then ignore the line.
+					continue;
+				}
+				
+				String[] chunks = line.split("\t");				
+				classOfObservations.add(chunks[classIndex]);
+			}
+		}
+		catch (IOException e)
+		{
+			// Caught an error while reading the file. Indicate this and exit.
+			System.out.println("An error occurred while determining the class of each observation.");
+			e.printStackTrace();
+			System.exit(0);
+		}
+		
+		return classOfObservations;
+	}
+	
+}
---
+++ RandomJyrest/src/analysis/WeightAndMtryOptimisation.java
@@ -0,0 +1,265 @@
+package analysis;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+import randomjyrest.Forest;
+
+public class WeightAndMtryOptimisation
+{
+
+	/**
+	 * Used in the optimisation of the mtry parameter and the weights of the individual classes.
+	 * 
+	 * @param args - The file system locations of the files and directories used in the optimisation.
+	 */
+	public static void main(String[] args)
+	{
+		String inputFile = args[0];  // The location of the dataset used to grow the forests.
+		String resultsDir = args[1];  // The location where the results of the optimisation will be written.
+		main(inputFile, resultsDir);
+	}
+
+	/**
+	 * @param inputFile - The location of the dataset used to grow the forests.
+	 * @param resultsDir - The location where the results of the optimisation will be written.
+	 */
+	public static void main(String inputFile, String resultsDir)
+	{
+		//===================================================================
+		//==================== CONTROL PARAMETER SETTING ====================
+		//===================================================================
+		int numberOfForestsToCreate = 100;  // The number of forests to create for each weight/mtry combination.
+		int numberOfTreesToGrow = 1000;  // The number of trees to grow in each forest.
+		boolean isCalculateOOB = true;  // OOB error is being calculated.
+		Integer[] mtryToUse = {5, 10, 15, 20, 25, 30};  // The different values of mtry to test.
+		
+		String[] unusedFeatures = new String[]{"UPAccession"};
+		List<String> featuresToRemove = Arrays.asList(unusedFeatures);
+		int numberOfProcesses = 4;
+		
+		double[] positiveWeightsToTest = new double[]{1.0};
+		double[] unlabelledWeightsToTest = new double[]{1.0};
+		//===================================================================
+		//==================== CONTROL PARAMETER SETTING ====================
+		//===================================================================
+
+		// Setup the directory for the results.
+		File resultsDirectory = new File(resultsDir);
+		if (!resultsDirectory.exists())
+		{
+			// The results directory does not exist.
+			boolean isDirCreated = resultsDirectory.mkdirs();
+			if (!isDirCreated)
+			{
+				System.out.println("The results directory does not exist, but could not be created.");
+				System.exit(0);
+			}
+		}
+		else
+		{
+			// The results directory already exists.
+			System.out.println("The results directory already exists. Please remove/rename the file before retrying");
+			System.exit(0);
+		}
+
+		// Initialise the results, parameters and controller object record files.
+		String resultsLocation = resultsDir + "/Results.txt";
+		String parameterLocation = resultsDir + "/Parameters.txt";
+		try
+		{
+			// Setup the results file.
+			FileWriter resultsOutputFile = new FileWriter(resultsLocation);
+			BufferedWriter resultsOutputWriter = new BufferedWriter(resultsOutputFile);
+			resultsOutputWriter.write("PositiveWeight\tUnlabelledWeight\tMtry\tGMean\tMCC\tF0.5\tF1\tF2\tAccuracy\tError\tTimeTakenPerRepetition(ms)\tPositives\t\tUnlabelleds\t");
+			resultsOutputWriter.newLine();
+			resultsOutputWriter.write("\t\t\t\t\t\t\t\t\t\t\tTrue\tFalse\tTrue\tFalse");
+			resultsOutputWriter.newLine();
+			resultsOutputWriter.close();
+
+			// Record the parameters.
+			FileWriter parameterOutputFile = new FileWriter(parameterLocation);
+			BufferedWriter parameterOutputWriter = new BufferedWriter(parameterOutputFile);
+			parameterOutputWriter.write("Number of forests grown - " + Integer.toString(numberOfForestsToCreate));
+			parameterOutputWriter.newLine();
+			parameterOutputWriter.write("Weights used");
+			parameterOutputWriter.newLine();
+			parameterOutputWriter.write("\tPositive - " + Arrays.toString(positiveWeightsToTest));
+			parameterOutputWriter.newLine();
+			parameterOutputWriter.write("\tUnlabelled - " + Arrays.toString(unlabelledWeightsToTest));
+			parameterOutputWriter.newLine();
+			parameterOutputWriter.write("Mtry used - " + Arrays.toString(mtryToUse));
+			parameterOutputWriter.newLine();
+			parameterOutputWriter.close();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			System.exit(0);
+		}
+		
+		// Determine the class of each observation.
+		List<String> classOfObservations = PredictionAnalysis.determineClassOfObservations(inputFile);
+		int numberOfObservations = classOfObservations.size();
+
+		// Generate all the random seeds to use in growing the forests. The same numberOfForestsToCreate seeds will be used for every weight/mtry
+		// combination. This ensures that the only difference in the results is due to the chosen weight/mtry combination.
+		Random randGen = new Random();
+		List<Long> seeds = new ArrayList<Long>();
+		for (int i = 0; i < numberOfForestsToCreate; i++)
+		{
+			long seedToUse = randGen.nextLong();
+			while (seeds.contains(seedToUse))
+			{
+				seedToUse = randGen.nextLong();
+			}
+			seeds.add(seedToUse);
+		}
+
+		for (int mtry : mtryToUse)
+		{
+			System.out.format("Now working on mtry - %d ", mtry);
+			DateFormat sdfDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		    Date currentTime = new Date();
+		    String strDate = sdfDate.format(currentTime);
+		    System.out.format("at %s.\n", strDate);
+			
+			for (double pWeight : positiveWeightsToTest)
+			{
+				System.out.format("\tNow working on positive weight - %f ", pWeight);
+				sdfDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			    currentTime = new Date();
+			    strDate = sdfDate.format(currentTime);
+			    System.out.format("at %s.\n", strDate);
+				
+				for (double uWeight : unlabelledWeightsToTest)
+				{
+					System.out.format("\t\tNow working on unlabelled weight - %f ", uWeight);
+					sdfDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+				    currentTime = new Date();
+				    strDate = sdfDate.format(currentTime);
+				    System.out.format("at %s.\n", strDate);
+					
+					// Determine the weight vector for this positive/unlabelled weight combination.
+					double[] weights = determineObservationWeights(classOfObservations, "Positive", pWeight, "Unlabelled", uWeight);
+					
+					// Setup the prediction output.
+					Map<String, double[]> predictions = new HashMap<String, double[]>();
+					predictions.put("Positive", new double[classOfObservations.size()]);
+					predictions.put("Unlabelled", new double[classOfObservations.size()]);
+					
+					long timeTaken = 0l;
+					for (int i = 0; i < numberOfForestsToCreate; i++)
+					{
+						// Grow the forest.
+						Date startTime = new Date();
+						Forest forest = new Forest();
+						Map<String, double[]> predictionsFromForest = forest.main(inputFile, numberOfTreesToGrow, mtry, featuresToRemove, weights, numberOfProcesses, isCalculateOOB);
+						Date endTime = new Date();
+						timeTaken += (endTime.getTime() - startTime.getTime());
+						
+						// Add the predictions from this forest to the aggregate results.
+						for (Map.Entry<String, double[]> entry : predictionsFromForest.entrySet())
+						{
+							String classOfPredictions = entry.getKey();
+							double[] oldPredictedWeights = predictions.get(classOfPredictions);
+							double[] newPredictedWeights = entry.getValue();
+							for (int j = 0; j < numberOfObservations; j++)
+							{
+								oldPredictedWeights[j] += newPredictedWeights[j];
+							}
+						}
+					}
+					
+					// Evaluate the aggregate predictions.
+					Map<String, Map<String, Integer>> confusionMatrix = PredictionAnalysis.calculateConfusionMatrix(classOfObservations, predictions);
+					
+					// Record the results of this weight combination.
+					try
+					{
+						FileWriter resultsOutputFile = new FileWriter(resultsLocation, true);
+						BufferedWriter resultsOutputWriter = new BufferedWriter(resultsOutputFile);
+						resultsOutputWriter.write(String.format("%.5f", pWeight));
+						resultsOutputWriter.write("\t");
+						resultsOutputWriter.write(String.format("%.5f", uWeight));
+						resultsOutputWriter.write("\t");
+						resultsOutputWriter.write(Integer.toString(mtry));
+						resultsOutputWriter.write("\t");
+						resultsOutputWriter.write(String.format("%.5f", PredictionAnalysis.calculateGMean(confusionMatrix, classOfObservations)));
+						resultsOutputWriter.write("\t");
+						resultsOutputWriter.write(String.format("%.5f", PredictionAnalysis.calculateMCC(confusionMatrix)));
+						resultsOutputWriter.write("\t");
+						resultsOutputWriter.write(String.format("%.5f", PredictionAnalysis.calculateFMeasure(confusionMatrix, classOfObservations, 0.5)));
+						resultsOutputWriter.write("\t");
+						resultsOutputWriter.write(String.format("%.5f", PredictionAnalysis.calculateFMeasure(confusionMatrix, classOfObservations, 1.0)));
+						resultsOutputWriter.write("\t");
+						resultsOutputWriter.write(String.format("%.5f", PredictionAnalysis.calculateFMeasure(confusionMatrix, classOfObservations, 2.0)));
+						resultsOutputWriter.write("\t");
+						double accuracy = PredictionAnalysis.calculateAccuracy(confusionMatrix);
+						resultsOutputWriter.write(String.format("%.5f", accuracy));
+						resultsOutputWriter.write("\t");
+						resultsOutputWriter.write(String.format("%.5f", 1 - accuracy));
+						resultsOutputWriter.write("\t");
+						resultsOutputWriter.write(Long.toString(timeTaken));
+						resultsOutputWriter.write("\t");
+						resultsOutputWriter.write(Double.toString(confusionMatrix.get("Positive").get("Correct")));
+						resultsOutputWriter.write("\t");
+						resultsOutputWriter.write(Double.toString(confusionMatrix.get("Positive").get("Incorrect")));
+						resultsOutputWriter.write("\t");
+						resultsOutputWriter.write(Double.toString(confusionMatrix.get("Unlabelled").get("Correct")));
+						resultsOutputWriter.write("\t");
+						resultsOutputWriter.write(Double.toString(confusionMatrix.get("Unlabelled").get("Incorrect")));
+						resultsOutputWriter.write("\t");
+						resultsOutputWriter.newLine();
+						resultsOutputWriter.close();
+					}
+					catch (Exception e)
+					{
+						e.printStackTrace();
+						System.exit(0);
+					}
+				}
+			}
+		}
+	}
+	
+	/**
+	 * @param observationClasses
+	 * @param positiveClass
+	 * @param positiveWeight
+	 * @param unlabelledClass
+	 * @param unlabelledWeight
+	 * @return
+	 */
+	private static final double[] determineObservationWeights(List<String> observationClasses, String positiveClass, double positiveWeight,
+			String unlabelledClass, double unlabelledWeight)
+	{
+		int numberOfObservations = observationClasses.size();
+		double[] weights = new double[numberOfObservations];
+		
+		for (int i = 0; i < numberOfObservations; i++)
+		{
+			String classOfObs = observationClasses.get(i);
+			if (classOfObs.equals(positiveClass))
+			{
+				weights[i] = positiveWeight;
+			}
+			else
+			{
+				weights[i] = unlabelledWeight;
+			}
+		}
+		
+		return weights;
+	}
+}
