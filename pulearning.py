c613965a58f2fee95f2de27aaebb89580d690382
unknown <SimonCB765@gmail.com>
2013-09-17 09:15:18 +0100

    Final changes

--- pulearning.py
+++ pulearning.py
@@ -10,11 +10,11 @@ def main(args):
 
     parser = argparse.ArgumentParser(description='Process the command line input for the PU learning.', epilog='Setting -b, --outfrac to a value x' +
         'will cause the top x fraction of the unlabelled proteins to be output as positive, and the others to be output as unlabelled')
-    parser.add_argument('dataset', help='the location containing the dataset')
-    parser.add_argument('output', help='the location to save the results')
+    parser.add_argument('dataset', help='the location of the file containing the dataset')
+    parser.add_argument('output', help='the output directory where the results should be saved')
     parser.add_argument('-f', '--features', default='', help='the features to remove (csv)')
     parser.add_argument('-a', '--possim', default=0.5, type=float, help='the fraction of positive similarities to remove (smaller similarities removed)')
-    parser.add_argument('-b', '--outfrac', default=0.0, type=float, help='the fraction of ulnabelled proteins to output as positive')
+    parser.add_argument('-b', '--outnum', default=0, type=int, help='the number of ulnabelled proteins to output as positive')
     parser.add_argument('-c', '--unlabsim', default=0.5, type=float, help='the fraction of unlabelled similarities to remove (smaller similarities removed)')
     args = parser.parse_args()
 
@@ -23,15 +23,14 @@ def main(args):
     resultsLocation = args.output
     featuresToRemove = args.features.split(',')
     fractionPositiveSimilarityToRemove = args.possim
-    fractionUnlabelledToConvert = args.outfrac
+    numberUnlabelledToConvert = args.outnum
     fractionUnlabelledSimilarityToRemove = args.unlabsim
 
     # Parse the file containing the dataset.
     proteinAccsAndClasses = numpy.genfromtxt(dataFileLocation, dtype=None, delimiter='\t', names=True, case_sensitive=True, usecols=[0,-1])
     unlabelledData = data_processing(dataFileLocation, featuresToRemove)
     unlabelledData = unlabelledData.view((numpy.float64, len(unlabelledData.dtype.names)))  # Convert the structured array created by genfromtxt to a regular array of floats.
-    numberOfOriginalFeatures = unlabelledData.shape[1]
-    unlabelledData = numpy.delete(unlabelledData, [0, numberOfOriginalFeatures - 1], 1)  # Remove the UPAccession and Classification columns.
+    numberOfFeatures = unlabelledData.shape[1]
 
     # Standardise the data.
     means = numpy.mean(unlabelledData, 0)
@@ -41,7 +40,6 @@ def main(args):
     # Determine the positive and unlabelled observations.
     positiveData = unlabelledData[proteinAccsAndClasses['Classification'] == b'Positive']
     unlabelledData = unlabelledData[proteinAccsAndClasses['Classification'] == b'Unlabelled']
-    numberOfFeatures = numberOfOriginalFeatures - 2
 
     # Determine the pairwise distances (Euclidean) between all positive observations.
     positiveDistances = numpy.zeros((positiveData.shape[0], positiveData.shape[0]))
@@ -56,7 +54,7 @@ def main(args):
     maxPositiveDistance = sortedPositiveDistances[-1][0]
     minPositiveDistance = sortedPositiveDistances[0][0]
 
-    # Set the fractionPositiveSimilarityToRemove smallest simliarities to the largest distance (this will cause them to be set to 0 after the scaling).
+    # Truncate the fractionPositiveSimilarityToRemove largest distances (this will cause them to be set to 0 after the scaling).
     if fractionPositiveSimilarityToRemove > 0:
         numberOfDistancesToRemove = int(len(sortedPositiveDistances) * fractionPositiveSimilarityToRemove)
         maxPositiveDistance = sortedPositiveDistances[-numberOfDistancesToRemove][0]
@@ -84,7 +82,7 @@ def main(args):
     maxUnlabelledDistance = sortedUnlabelledDistances[-1][0]
     minUnlabelledDistance = sortedUnlabelledDistances[0][0]
 
-    # Set the fractionUnlabelledSimilarityToRemove smallest simliarities to the largest distance (this will cause them to be set to 0 after the scaling).
+    # Truncate the fractionUnlabelledSimilarityToRemove largest distances (this will cause them to be set to 0 after the scaling).
     if fractionUnlabelledSimilarityToRemove > 0:
         numberOfDistancesToRemove = int(len(sortedUnlabelledDistances) * fractionUnlabelledSimilarityToRemove)
         maxUnlabelledDistance = sortedUnlabelledDistances[-numberOfDistancesToRemove][0]
@@ -99,7 +97,63 @@ def main(args):
     # Determine the positive likeness each of the unlabelled observations.
     positiveWeightings = numpy.dot(positiveDistances, unlabelledDistances)
     positiveLikeness = numpy.sum(positiveWeightings, 0)
-    print(positiveLikeness)
+
+    # Scale the positive likenesses to be between 0 (least like a positive) to 1 (most like  positive).
+    minLikeness = numpy.min(positiveLikeness)
+    maxLikeness = numpy.max(positiveLikeness)
+    positiveLikeness = (positiveLikeness - minLikeness) / (maxLikeness - minLikeness)
+
+    # Output the parameters.
+    fileParameters = resultsLocation + '/ParametersUsed.txt'
+    writeParams = open(fileParameters, 'w')
+    writeParams.write('Features Removed : ' + args.features + '\n')
+    writeParams.write('Fraction of lagest positive dstances truncated = {0:0.2f}\n'.format(args.possim))
+    writeParams.write('Fraction of lagest unlabelled dstances truncated = {0:0.2f}\n'.format(args.unlabsim))
+    writeParams.close()
+
+    # Output the unlabelled UniProt accession along with their scaled likeness in descnding order of likeness.
+    fileUPAccessionLikeness = resultsLocation + '/UPAccessionLikeness.txt'
+    writeLikeness = open(fileUPAccessionLikeness, 'w')
+    orderedLikenesses = sorted(zip(positiveLikeness, proteinAccsAndClasses[proteinAccsAndClasses['Classification'] == b'Unlabelled']['UPAccession']), key=lambda x : x[0], reverse=True)
+    for i,j in orderedLikenesses:
+        # The UniProt accessions are recorded as binary strings, so need to convert each one to an ascii string.
+        writeLikeness.write('{0}\t{1:0.2f}\n'.format(''.join([chr(k) for k in j]), i))
+    writeLikeness.close()
+
+    # Output the weight discount for all positive and unlabelled proteins.
+    fileAllProteinWeightings = resultsLocation + '/ProteinWeightings.txt'
+    writeWeightings = open(fileAllProteinWeightings, 'w')
+    currentUnlabelledIndex = 0
+    for i in proteinAccsAndClasses:
+        proteinClass = i[1]
+        if proteinClass == b'Positive':
+            writeWeightings.write('1.00\n')
+        else:
+            writeWeightings.write('{0:0.2f}\n'.format(1 - positiveLikeness[currentUnlabelledIndex]))  # The discount is 1 - the likeness as you want a discount of 0 for the unlabelled that is most like a positive.
+            currentUnlabelledIndex += 1
+    writeWeightings.close()
+
+    # Output the new dataset.
+    proteinsToConvert = [''.join([chr(j) for j in i[1]]) for i in orderedLikenesses[:numberUnlabelledToConvert]]  # Get the accessions of the numberUnlabelledToConvert most positive-like unlabelled proteins.
+    fileNewDataset = resultsLocation + '/Dataset_Converted_{0:d}.txt'.format(numberUnlabelledToConvert)
+    writeDataset = open(fileNewDataset, 'w')
+    readDataset = open(dataFileLocation, 'r')
+    writeDataset.write(readDataset.readline())  # Record the header.
+    for line in readDataset:
+        chunks = (line.strip()).split('\t')
+        if chunks[-1] == 'Positive':
+            # If the protein is positive, then just output it.
+            writeDataset.write(line)
+        else:
+            if chunks[0] in proteinsToConvert:
+                # If the protein is unlabelled, and should be converted, then change its clsas to positive.
+                chunks[-1] = 'Positive'
+                writeDataset.write('\t'.join(chunks) + '\n')
+            else:
+                # If the protein is unlabelled, and not to be converted, then just output it.
+                writeDataset.write(line)
+    readDataset.close()
+    writeDataset.close()
 
 def data_processing(dataFileLocation, featuresToRemove=[]):
     """Process a data file.
