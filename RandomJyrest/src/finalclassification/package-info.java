bea39b9c54c1dd97f3e256e09c47f41a528fbb84
unknown <SimonCB765@gmail.com>
2013-09-19 12:33:05 +0100

    Updated package info

---
+++ RandomJyrest/src/finalclassification/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Produce a final prediction of the class of each observation.
+ * 
+ * The prediction output for each observation is the prediction weight for each class. The final prediction can then be computed
+ * from these weights in any manner desired.
+ * 
+ * @author Simon Bull
+ */
+package finalclassification;
