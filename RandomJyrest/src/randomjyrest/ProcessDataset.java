ad7f16d7b0d1909af3df54d6db58641e21837fa0
unknown <SimonCB765@gmail.com>
2013-10-08 14:33:29 +0100

    Update to the comments and documentation of the dataset processing classes

--- RandomJyrest/src/randomjyrest/ProcessDataset.java
+++ RandomJyrest/src/randomjyrest/ProcessDataset.java
@@ -30,44 +30,50 @@ public final class ProcessDataset
 	 * The features in featuresToRemove are not recorded in the final processed dataset. Additionally, any features that have
 	 * the same value for every observation are not recorded in the final processed dataset.
 	 * 
-	 * The values in the weight vector are the values for the individual observations. The values in the weight vector should
-	 * be specified so that weights[i] is the weight for the ith observation in the dataset. If the length of the weight vector
-	 * is less than the number of observations, then the weight vector is padded with 1.0s to make it have one value for each
-	 * observation.
+	 * The values in the weight vector are the values for the individual observations. The values should
+	 * be specified so that weights[i] is the weight for the ith observation in the dataset (i.e. the ith observaton in the input file).
+	 * If the length of the weight vector is less than the number of observations, then the weight vector is padded with 1.0s to make
+	 * it have one value for each observation.
 	 * 
 	 * The data file is expected to be tab separated with the first line containing the names of the features/columns.
-	 * The restrictions on the naming of the features/columns are:
-	 *	1) The column containing the class should be headed with Classification.
-	 *	2) There should not be a feature/column named Weight.
+	 * The column in the file containing the class should be headed with Classification.
 	 *
-	 * The processed data has the form:
-	 * 		featureName ->
-	 * 						"Data" 	- The array containing the values for the feature.
-	 * 						"Index" - The array containing the indices of the observations.
-	 * returnValue.get(featureName).get("Data") will contain the values of the feature sorted in ascending order. The values in
-	 * returnValue.get(featureName).get("Index") are also sorted, but the sorting is done based on the data values.
+	 * The processed data has three components:
+	 * 		1) A mapping from the name of each feature, F, to the values of the feature for each observation. The values of the observations
+	 * 		   are sorted in ascending order.
+	 * 		2) A mapping from the name of each feature, F, to an array containing the original indices in the input file of the sorted
+	 * 		   observation values for the feature. For example, if the smallest observation value comes from the 3rd observation in
+	 * 		   the input file, then the 0th entry in this component for F will be 2.
+	 * 		3) A mapping from each class to an array containing the weight of each observation for the class.
 	 * Example:
-	 * 		The values of a feature are		[4, 7, 2, 8, 3, 4]
-	 * 		The indices are					[0, 1, 2, 3, 4, 5]
-	 * 		The sorted values are			[2, 3, 4, 4, 7, 8]
-	 * 		The sorted indices are			[2, 4, 0, 5, 1, 3]
-	 * 		The value 8 is therefore the greatest data value, and comes from the observation with index 3 in the original data file.
+	 * 		The values of a feature, F, in the input file are		[4, 7, 2, 8, 3, 4]
+	 * 		The indices of the values are							[0, 1, 2, 3, 4, 5]
+	 * 		The classes of the observations are						[A, B, B, C, A, C]
+	 * 		The weights for the observations are					[3, 2, 7, 4, 1, 1]
+	 * 
+	 * 		1) The array stored at F for component 1 is				[2, 3, 4, 4, 7, 8]	(sorted observation values)
+	 * 		2) The array stored at F for component 2 is				[2, 4, 0, 5, 1, 3]	(the original indices of the sorted values)
+	 * 		3) The array stored at
+	 * 			A for component 3 is								[3, 0, 0, 0, 1, 0]	(each observation's weight for class A)
+	 * 			B for component 3 is								[0, 2, 7, 0, 0, 0]	(each observation's weight for class B)
+	 * 			C for component 3 is								[0, 0, 0, 4, 0, 1]	(each observation's weight for class C)
 	 * 
 	 * @param dataset			The location of the file containing the data to be processed.
 	 * @param featuresToRemove	The features in the dataset that should be removed (not processed).
 	 * @param weights			The weights of the individual observations.
-	 * @return					A mapping from the feature names to two arrays. One is the "Data" array. This contains the sorted
-	 * 							values for the feature. The other is the "Index" array. This contains the indices of the observations
-	 * 							sorted by the ordering in the "Data" array.
+	 * @return					Three mappings, one from feature names to sorted data values, one from feature names to original
+	 * 							indices of the sorted data values and one from class names to the weight of each observation to the class. 
 	 */
 	public static final ImmutableThreeValues<Map<String, double[]>, Map<String, int[]>, Map<String, double[]>>
 		main(String dataset, List<String> featuresToRemove, double[] weights)
 	{
 		// Setup the mapping to hold the temporary and final processed data.
-		Map<String, List<Double>> temporaryData = new HashMap<String, List<Double>>();
-		Map<String, double[]> processedFeatureData = new HashMap<String, double[]>();
-		Map<String, int[]> processedIndexData = new HashMap<String, int[]>();
-		Map<String, double[]> processedClassData = new HashMap<String, double[]>();
+		Map<String, List<Double>> temporaryData = new HashMap<String, List<Double>>();  // Mapping to hold the raw extracted data.
+		Map<String, double[]> processedFeatureData = new HashMap<String, double[]>();  // Mapping to hold the sorted data values.
+		Map<String, int[]> processedIndexData = new HashMap<String, int[]>();  // Mapping to hold the original indices of the data values.
+		Map<String, double[]> processedClassData = new HashMap<String, double[]>();  // Mapping to hold the class weight of each observation.
+
+		int numberOfObservations = 0;  // The number of observations in the input file.
 
 		BufferedReader reader = null;
 		try
@@ -75,7 +81,8 @@ public final class ProcessDataset
 			reader = new BufferedReader(new FileReader(dataset));
 			String line = null;
 
-			// Generate a mapping from the index of the column in the dataset to the name of the feature that the column contains values of.
+			// Generate a mapping from the index of the column in the dataset to the name of the feature that the column contains
+			// values of.
 			line = reader.readLine();
 			line = line.replaceAll("\n", "");
 			String[] featureNames = line.split("\t");
@@ -83,17 +90,19 @@ public final class ProcessDataset
 			
 			// Initialise the mapping that holds the temporary processing of the data.
 			List<Integer> featureIndicesToUse = new ArrayList<Integer>();  // The indices of the columns in the file which are not to be removed.
-			List<String> classData = new ArrayList<String>();
-			int classIndex = -1;
-			int featureIndex = 0;
+			List<String> classData = new ArrayList<String>();  // The class of each observation ordered as the observations are in the file.
+			int classIndex = -1;  // The index of the column containing the class of the observations.
+			int featureIndex = 0;  // The current index of the feature.
 			for (String feature : featureNames)
 			{
 				if (feature.equals(classFeatureColumnName))
 				{
+					// Record if the feature is actually the class column.
 					classIndex = featureIndex;
 				}
 				else if (!featuresToRemove.contains(feature))
 				{
+					// If the feature is not one to be removed, then record the index of the column as one to extract data from.
 					featureIndicesToUse.add(featureIndex);
 					temporaryData.put(feature, new ArrayList<Double>());
 				}
@@ -107,7 +116,7 @@ public final class ProcessDataset
 				System.exit(0);
 			}
 			
-			int currentObservationIndex = -1;
+			int currentObservationIndex = -1;  // The index of the current observation.
 			while ((line = reader.readLine()) != null)
 			{
 				line = line.trim();
@@ -145,7 +154,7 @@ public final class ProcessDataset
 				}
 				weights = newWeightVector;
 			}
-			int numberOfObservations = weights.length;
+			numberOfObservations = weights.length;
 
 			// Setup the class information.
 			Set<String> classesInDataset = new HashSet<String>(classData);
@@ -193,17 +202,18 @@ public final class ProcessDataset
 		}
 
 		// Generate the final processed data.
-		int numberOfObservations = weights.length;
 		for (Map.Entry<String, List<Double>> entry : temporaryData.entrySet())
 		{
+			// Sort the values of each observaton separately for each feature.
 			String feature = entry.getKey();
 			List<Double> data = entry.getValue();
 			List<IndexedDoubleData> sortedData = new ArrayList<IndexedDoubleData>();
 			for (int i = 0; i < numberOfObservations; i++)
 			{
+				// Add all the observation values along with their original index.
 				sortedData.add(new IndexedDoubleData(data.get(i).doubleValue(), i));
 			}
-			Collections.sort(sortedData);
+			Collections.sort(sortedData);  // Sort the value-index pairs in ascending order by value.
 			
 			if (sortedData.get(0).getData() == sortedData.get(numberOfObservations - 1).getData())
 			{
@@ -212,14 +222,15 @@ public final class ProcessDataset
 				continue;
 			}
 			
-			double[] sortedFeatureData = new double[numberOfObservations];
-			int[] sortedFeatureIndices = new int[numberOfObservations];
+			double[] sortedFeatureData = new double[numberOfObservations];  // Initialise the sorted data value.
+			int[] sortedFeatureIndices = new int[numberOfObservations];  // Initialise the original indices.
 			for (int i = 0; i < numberOfObservations; i++)
 			{
 				sortedFeatureData[i] = sortedData.get(i).getData();
 				sortedFeatureIndices[i] = sortedData.get(i).getIndex();
 			}
 			
+			// Put the data values and original indices into the return value mappings.
 			processedFeatureData.put(feature, sortedFeatureData);
 			processedIndexData.put(feature, sortedFeatureIndices);
 		}
--- RandomJyrest/src/randomjyrest/ProcessPredictionData.java
+++ RandomJyrest/src/randomjyrest/ProcessPredictionData.java
@@ -13,20 +13,41 @@ import utilities.ImmutableTwoValues;
 public class ProcessPredictionData
 {
 
+	/**
+	 * Processes a file containing a dataset of observations.
+	 * 
+	 * Processes a tsv format file, while ensuring that only the desired features in the dataset are in the processed dataset.
+	 * 
+	 * The values for the features in the dataset are all assumed to be numeric (e.g. integers or reals) rather than strings
+	 * representing categories. Any categorical data should therefore be mapped to a set of integers (or reals), and will then
+	 * be treated as if it was numeric not categorical (i.e. binary splits will always be performed).
+	 * 
+	 * The features in featuresToRemove are not recorded in the final processed dataset.
+	 * 
+	 * The data file is expected to be tab separated with the first line containing the names of the features/columns.
+	 * If a column headed Classification is present, then that column is ignored (as it is reserved for recording the class of observations).
+	 * 
+	 * @param dataset			The location of the file containing the data to be processed.
+	 * @param featuresToRemove	The features in the dataset that should be removed (not processed).
+	 * @return					A mapping from each feature name to the values of the observations for it, along with the number
+	 * 							of observations.
+	 */
 	public static final ImmutableTwoValues<Map<String, double[]>, Integer> main(String dataset, List<String> featuresToRemove)
 	{
 		// Setup the mapping to hold the temporary and final processed data.
-		Map<String, List<Double>> temporaryData = new HashMap<String, List<Double>>();
-		Map<String, double[]> datasetToPredict = new HashMap<String, double[]>();
+		Map<String, List<Double>> temporaryData = new HashMap<String, List<Double>>();  // Mapping to hold the raw extracted data.
+		Map<String, double[]> datasetToPredict = new HashMap<String, double[]>();  // Mapping to hold the processed data.
 		
-		int numberOfObservations = 0;
+		int numberOfObservations = 0;  // The number of observations in the dataset.
+
 		BufferedReader reader = null;
 		try
 		{
 			reader = new BufferedReader(new FileReader(dataset));
 			String line = null;
 			
-			// Generate a mapping from the index of the column in the dataset to the name of the feature that the column contains values of.
+			// Generate a mapping from the index of the column in the dataset to the name of the feature that the column contains
+			// values of.
 			line = reader.readLine();
 			line = line.replaceAll("\n", "");
 			String[] featureNames = line.split("\t");
@@ -34,16 +55,16 @@ public class ProcessPredictionData
 			
 			// Initialise the mapping that holds the temporary processing of the data.
 			List<Integer> featureIndicesToUse = new ArrayList<Integer>();  // The indices of the columns in the file which are not to be removed.
-			int featureIndex = 0;
+			int featureIndex = 0;  // The current index of the feature.
 			for (String feature : featureNames)
 			{
 				if (feature.equals(classFeatureColumnName))
 				{
-					;  // Ignore the class column
+					;  // Ignore the class column if there is one.
 				}
 				else if (!featuresToRemove.contains(feature))
 				{
-					// If the feature is not one to be removed from the dataset.
+					// If the feature is not one to be removed, then record the index of the column as one to extract data from.
 					featureIndicesToUse.add(featureIndex);
 					temporaryData.put(feature, new ArrayList<Double>());
 				}
