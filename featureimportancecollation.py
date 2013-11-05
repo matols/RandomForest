e31db4ccaa8572891e44b162839d70901f261cdc
unknown <SimonCB765@gmail.com>
2013-11-05 19:18:15 +0000

    Update to remove the GA run fraction from the feature importance collation

--- featureimportancecollation.py
+++ featureimportancecollation.py
@@ -10,16 +10,14 @@ def main(args):
     parser.add_argument('statTest', help='the location containing the results of the statistical significance testing')
     parser.add_argument('varImp', help='the location containing the results of the variable importance calculations')
     parser.add_argument('output', help='the location to save the results')
-    parser.add_argument('-g', '--ga', default=None, help='the location containing the results of the genetic algorithm runs')
     args = parser.parse_args()
 
     # Parse the command line arguments.
     statisticalTestFile = args.statTest  # The file containing the results of the statistical testing.
     variableImportanceFile = args.varImp  # The file containing the results of the random forest variable importance calculations.
-    gaAnalysisFile = args.ga  # The file containing the results of the repeated genetic algorithm runs.
     resultsLocation = args.output  # The location of the file where the collated feature importance measures should be written.
 
-    featureDict = {}  # A dictionary containing the feature importanc measures for each feature.
+    featureDict = {}  # A dictionary containing the feature importance measures for each feature.
 
     # Parse the results of the statistical testing.
     readStats = open(statisticalTestFile, 'r')
@@ -33,80 +31,50 @@ def main(args):
     # Parse the results of the variable importance calculations.
     readImp = open(variableImportanceFile, 'r')
     varImpFeaturesTested = (readImp.readline()).strip().split('\t')
-    variableImpLevels = dict([(i, []) for i in varImpFeaturesTested])  # Dictionary to hold the variable immportance levels for each feature.
+    variableImportances = dict([(i, []) for i in varImpFeaturesTested])  # Dictionary to hold the variable immportances for each feature.
     for line in readImp:
         chunks = (line.strip()).split('\t')
         for i in range(len(chunks)):
-            variableImpLevels[varImpFeaturesTested[i]].append(chunks[i])
+            variableImportances[varImpFeaturesTested[i]].append(chunks[i])
     readImp.close()
 
     # Update featureDict to contain any features that were not tested for statistical significance, but do have a variable importance.
     featureDict = dict([(i, featureDict[i] if i in featureDict else i + '\t-\t-\t-\t-\t') for i in varImpFeaturesTested])
 
     # Update featureDict with the variable importances.
-    for i in variableImpLevels:
-        meanLevel, medianLevel, maxLevel, minLevel, stdDevLevel, rangeLevel = calculate_level_stats(variableImpLevels[i])
-        featureDict[i] += str(meanLevel) + '\t' + str(medianLevel) + '\t' + str(maxLevel) + '\t' + str(minLevel) + '\t' + str(stdDevLevel) + '\t' + str(rangeLevel) + '\t'
-
-    # Parse the genetic algorithm run results. The genetic algorithm and variable improtance claculations are assumed to have been done with the same
-    # set of features.
-    if gaAnalysisFile:
-        readGA = open(gaAnalysisFile, 'r')
-        for line in readGA:
-            line = line.strip()
-            if not line:
-                # If the line is blank, then skip it.
-                continue
-            chunks = line.split('\t')
-            feature = chunks[0]
-            if feature in ['Fitness', 'Seed']:
-                # If the first entry on the line is Fitness or Seed, then the line does not contain information about a feature.
-                continue
-            featureDict[feature] += chunks[-1] + '\n'
-        readGA.close()
-    else:
-        for feature in featureDict:
-            featureDict[feature] += 'X\n'
+    for i in variableImportances:
+        meanImportance, maxImportance, minImportance, stdDevImportance, rangeImportance = calculate_level_stats(variableImportances[i])
+        featureDict[i] += str(meanImportance) + '\t' + str(maxImportance) + '\t' + str(minImportance) + '\t' + str(stdDevImportance) + '\t' + str(rangeImportance) + '\t'
 
     # Write out the collated feature importance.
     writeTo = open(resultsLocation, 'w')
-    writeTo.write('Feature\tPValue\tSignificantAt-0.05\tSignificantAt-0.01\tCorrectedSignificantAt-0.05\tMeanImpLevel\tMedianImpLevel\tMaxImpLevel\tMinImpLevel\tStdDevImpLevel\tRangeImpLevel\tFractionOfGARuns\n')
+    writeTo.write('Feature\tPValue\tSignificantAt-0.05\tSignificantAt-0.01\tCorrectedSignificantAt-0.05\tMeanImportance\tMaxImportance\tMinImportance\tStdDevImportance\tRangeImportance\n')
     for i in varImpFeaturesTested:
         writeTo.write(featureDict[i])
     writeTo.close()
 
-def calculate_level_stats(levels):
-    """Calculate the statistics of the variable importance levels for a given feature.
+def calculate_level_stats(importances):
+    """Calculate the statistics of the variable importances for a given feature.
 
-    :parma levels: the importance levels of a feature
-    :type levels: list
+    :parma importances: the importances of a feature
+    :type importances: list
 
     """
 
-    levels = [int(i) for i in levels if i != '-']
-    meanLevel = sum(levels) / len(levels)
-    maxLevel = max(levels)
-    minLevel = min(levels)
-    rangeLevel = maxLevel - minLevel
+    importances = sorted([float(i) for i in importances])
+	importanceRepetitions = len(importances)
+    meanImportance = sum(importances) / importanceRepetitions
+    maxImportance = importances[-1]
+    minImportance = importances[0]
+    rangeImportance = maxImportance - minImportance
 
-    levels = sorted(levels)
-    numberLevels = len(levels)
+    stdDevImportance = 0
+    for i in importances:
+        stdDevImportance += (i - meanImportance) ** 2
+    stdDevImportance /= importanceRepetitions
+    stdDevImportance = stdDevImportance ** 0.5
 
-    medianLevel = 0
-    if numberLevels % 2 == 0:
-        midPointOne = numberLevels // 2
-        midPointTwo = midPointOne - 1
-        medianLevel = (levels[midPointOne] + levels[midPointTwo]) / 2
-    else:
-        medianLevel = levels[numberLevels // 2]
-
-    stdDevLevel = 0
-    for i in levels:
-        stdDevLevel += (i - meanLevel) ** 2
-    stdDevLevel /= numberLevels
-    stdDevLevel = stdDevLevel ** 0.5
-
-    return meanLevel, medianLevel, maxLevel, minLevel, stdDevLevel, rangeLevel
+    return meanImportance, maxImportance, minImportance, stdDevImportance, rangeImportance
 
 if __name__ == '__main__':
     main(sys.argv)
